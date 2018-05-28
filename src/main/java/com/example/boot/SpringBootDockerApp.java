2ad13ed0c2903cef0b5cf1dcf2886a6b1767f122
Syed Mansoor <syedmmansoor@gmail.com>
2018-05-28 12:42:30 +1000

    Removed unused code

--- src/main/java/com/example/boot/SpringBootDockerApp.java
+++ src/main/java/com/example/boot/SpringBootDockerApp.java
@@ -1,15 +1,11 @@
 package com.example.boot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootDockerApp {
-
-    private static final Logger log = LoggerFactory.getLogger(SpringBootDockerApp.class);
-
+    
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDockerApp.class, args);
     }
